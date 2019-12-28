915bd6842eff3c68a0f03db78ce43007381d3f07
Lakshyajit Laxmikant <30868587+lakshyajit165@users.noreply.github.com>
2019-12-29 00:04:17 +0530

    added cors filter bean to the main class

--- Demo1Application.java
+++ Demo1Application.java
@@ -11,5 +11,24 @@ public class Demo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    //config.setAllowCredentials(true); // you USUALLY want this
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("HEAD");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
+	    config.addAllowedMethod("PATCH");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
 
 }
